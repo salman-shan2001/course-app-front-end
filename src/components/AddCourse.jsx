a37b74b801eadfb85d25dfeed69f61ae33bbf22d
salman-shan2001 <salmanfarizy454@gmail.com>
2024-06-12 12:45:02 +0530

    updated

--- src/components/AddCourse.jsx
+++ src/components/AddCourse.jsx
@@ -76,6 +76,7 @@ const readvalue=()=>{
                     <button className="btn btn-success" onClick={readvalue}>ADD</button>
                 </div>
                 </center>
+                
             </div>
             </div>
         </div>
--- src/components/SearchCourse.jsx
+++ src/components/SearchCourse.jsx
@@ -1,38 +1,110 @@
-import React from 'react'
+import React, { useState } from 'react'
 import NAvbar from './NAvbar'
+import axios from 'axios'
 
 const SearchCourse = () => {
-  return (
+
+  const [course, changecourse] = useState(
+    {
+      "title": ""
+    }
+  )
+
+  const [result, changereselt] = useState([
+    {
+    
+      "title": "",
+      "description": "",
+      "date": "",
+      "duration": "",
+      "venue": "",
+      "trainer-name": ""
+    }
+  ]
+  
+)
+
+const inputHandler = (event) => {
+  changecourse({ ...course, [event.target.name]: event.target.value })
+}
+
+const readvalue = () => {
+  console.log(course)
+  axios.post("http://localhost:8055/search",course ).then(
+   (response)=>{
+    changereselt(response.data)
+   }
+  ).catch(
+    (error)=>{
+      console.log(error)
+    }
+  ) 
+
+}
+return (
+  <div>
     <div>
-      <div>
       <div className="container">
         <div className="row">
-            <NAvbar/>
-            <center>
-                <h1>
-                    Search Course
-                </h1>
-            </center>
-            <br></br><br></br>
-            <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+          <NAvbar />
+          <center>
+            <h1>
+              Search Course
+            </h1>
+          </center>
+          <br></br><br></br>
+          <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
             <div className="row g-3">
+              <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                <label htmlFor="" className="form-label">Title</label>
+                <input type="text" className="form-control" name='title' value={course.title} onChange={inputHandler} />
+              </div>
+
+              <center>
                 <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
-                    <label htmlFor="" className="form-label">Title</label>
-                    <input type="text" className="form-control" />
+                  <button className="btn btn-primary" onClick={readvalue}>SEARCH</button>
                 </div>
-                
-                <center>
-                <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
-                    <button className="btn btn-primary">SEARCH</button>
-                </div>
-                </center>
+              </center>
             </div>
-            </div>
+          </div>
         </div>
       </div>
+      <div className="row">
+        <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+          <table class="table">
+            <thead>
+              <tr>
+                <th scope="col">Title</th>
+                <th scope="col">Duration</th>
+                <th scope="col">Data</th>
+                <th scope="col">venue</th>
+              </tr>
+            </thead>
+            <tbody>
+             {
+              result.map(
+                (value,index)=>{
+                  return  <tr>
+                  <th scope="row">{value.title}</th>
+                  <td>{value.description}</td>
+                  <td>{value.date}</td>
+                  <td>{value.venue}</td>
+                  <td>
+                    <button className="btn btn-danger">DELETE</button>
+                  </td>
+                </tr>
+                }
+              )
+             }
+              
+            </tbody>
+          </table>
+        </div>
+
+      </div>
     </div>
-    </div>
-  )
+  </div>
+)
 }
 
 export default SearchCourse
